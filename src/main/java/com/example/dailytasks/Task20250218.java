ece2bb30e22c33b6c225db6d8918513d30007177
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2025-02-19 19:05:54 +0530

    Added Task20250218 file

---
+++ src/main/java/com/example/dailytasks/Task20250218.java
@@ -0,0 +1,8 @@
+package com.example.dailytasks;
+
+public class Task20250218 extends TaskTemplate {
+    @Override
+    public void execute() {
+        System.out.println("Task for 2025-02-18: Welcome to the first step of github");
+    }
+}
